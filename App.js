82f4d288ec1deacf4936ae0025866c48e58afb48
Lucas Zacarias <lzsd@cin.ufpe.br>
2020-08-28 10:31:16 -0300

    Entrega 2 Celo

--- App.js
+++ App.js
@@ -1,93 +1,100 @@
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-import { AppNavigator } from './src/navigation/index'
-
-import * as firebase from 'firebase';
-import { firebaseConfig } from './config';
-// Before rendering any navigation stack
-import { enableScreens } from 'react-native-screens';
-import {YellowBox} from 'react-native';
-
-import { ApplicationProvider, Layout, Text, IconRegistry } from '@ui-kitten/components';
-import { mapping, light, dark } from "@eva-design/eva";
-const themes = { light, dark };
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import React, { useEffect } from 'react';
 import * as eva from '@eva-design/eva';
-import { default as customTheme } from './src/shared/custom-theme.json'; // <-- Import app theme
-import { ThemeContext } from './theme-context';
-// By using react-native-screens, it is possible for each native platform to 
-// optimize the memory usage for screens that are under the view stack and also simplify the native node hierarchy. 
-enableScreens();
+import { AppRegistry , StatusBar} from "react-native";
+import {ThemeContext} from './theme-context';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import {mapping, light, dark} from '@eva-design/eva';
+import { default as customTheme } from './custom-theme.json';
+import {default as customMapping} from './custom-mapping.json'; 
+import { AppNavigator  } from "./src/navigation/navigation.component";
+import {ApplicationProvider, IconRegistry, Layout} from '@ui-kitten/components';
+import { Provider } from 'react-redux';
+import { store, persistor  } from './src/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import auth from '@react-native-firebase/auth';
+import AnimatedSplash from "react-native-animated-splash-screen";
+import { setFirstAccess } from './src/store/actions/auth';
+import { useSelector, useDispatch } from 'react-redux';
+const themes = {light, dark};
+console.disableYellowBox = true;
+import messaging from '@react-native-firebase/messaging';
 
-//Fix "Firebase App named '[DEFAULT]' already exists" issue
-if (!firebase.apps.length) {
-  // console.log("[App] Firebase.apps=", firebase.apps);
-  // console.log("[App] Ready to initializeApp firebase.apps.length=", firebase.apps.length);
-  firebase.initializeApp(firebaseConfig);
-} else {
-  // console.log("[App] Firebase.apps=", firebase.apps);
-  // console.log("[App] Ready to firebase.app() firebase.apps.length=", firebase.apps.length);
-  firebase.app();
-}
-// export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 
-// const fetchFonts = () => {
-//   return Font.loadAsync({
-//     'roboto-medium': require('./src/assets/fonts/Roboto-Medium.ttf')
-//   });
-// };
 
-export default function App() {
-
+const  StarterApp = () => {
+ 
+  const [isLoaded, setIsLoaded] = React.useState(false);
   const [theme, setTheme] = React.useState('light');
+  const currentTheme = themes[theme];
+ 
 
-  
+ 
+  useEffect(() => {
+    // Assume a message-notification contains a "type" property in the data payload of the screen to open
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.notification,
+      );
+      // Aqui podemos colocar ações, como ir para uma pagina
+      // navigation.navigate(remoteMessage.data.type);
+    });
+
+    // Check whether an initial notification is available
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+          // setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+        }
+        
+      });
+  }, []);
+
+
+  useEffect(() => {
+   
+    setTimeout(() => { setIsLoaded(true)}, 500);
+   }, []);
   const toggleTheme = () => {
     const nextTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(nextTheme);
   };
-  
-  // YellowBox.ignoreWarnings(['Warning: ReactNative.createElement']);
 
-  // const [fontLoaded, setFontLoaded] = useState(false);
-  // if (!fontLoaded) {
-  //   return (
-  //     <AppLoading
-  //       startAsync={fetchFonts}
-  //       onFinish={() => setFontLoaded(true)}
-  //       onError={(err) => console.log(err)}
-  //     />);
-  // }console.disableYellowBox = true;
-  console.disableYellowBox = true;
   return (
-    <>
-    
-    <IconRegistry icons = {EvaIconsPack}/>
-    <ThemeContext.Provider value={{ theme, toggleTheme }}> 
-    <ApplicationProvider  {...eva} theme={{ ...eva[theme], ...customTheme}}>
-      <AppNavigator />
-    </ApplicationProvider>
-    </ThemeContext.Provider>
-    </>
-  );
-}
+    <AnimatedSplash
+      isLoaded={isLoaded}
+      logoImage={require("./src/assets/images/icon.png")}
+      backgroundColor={"#fff"}
+      logoHeight={150}
+      logoWidth={150}
+    > 
+      <React.Fragment>
+        <IconRegistry icons={EvaIconsPack} />
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <ApplicationProvider
+                {...eva}
+                mapping={mapping}
+                theme={{ ...eva[theme], ...customTheme}}
+                customMapping={customMapping}>
+                <AppNavigator/> 
+              </ApplicationProvider>
+            </PersistGate>
+          </Provider>
+          </ThemeContext.Provider>
+      </React.Fragment>
+    </AnimatedSplash>
+    );
+  }
+ 
 
+AppRegistry.registerComponent("StarterApp", () => StarterApp);
 
-// const AppSwitchNavigator = createSwitchNavigator({
-//   LoadingScreen: { screen: LoadingScreen },
-//   LoginScreen: { screen: LoginScreen },
-//   DashboardScreen: { screen: DashboardScreen }
-// });
-
-// const AppNavigator = createAppContainer(AppSwitchNavigator);
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center'
-//   }
-// });
+export default StarterApp;
